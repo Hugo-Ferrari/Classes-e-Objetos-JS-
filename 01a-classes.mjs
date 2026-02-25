34ac96cd4bf1431c04fb3eb9558f43014db6ea8d
Hugo-Ferrari <hugoferrari36@gmail.com>
2026-02-25 20:41:19 -0300

    classes e objetos (estrutura de dados)

---
+++ 01a-classes.mjs
@@ -0,0 +1,61 @@
+// o "m" na frente do javaScript sig module; 
+
+// # sig que a classe é privada
+
+class formaGeometrica{
+    #base
+    #altura 
+    #tipo
+
+    constructor(base, altura, tipo){
+        this.#base = base
+        this.#altura= altura 
+        this.#tipo = tipo
+    }
+    // get = para listar ou visualizar 
+    get base(){
+        return this.#base
+    }
+
+    get altura(){
+        return this.#altura 
+    }
+    get tipo(){
+        return this.#tipo
+    }
+
+    //set = inserir valores
+    set base(valor){
+        if( typeof valor !== "number" || valor<0){
+            throw new Error("Erro: a base precisa ser numérica")
+        }
+        this.#base = valor
+    }
+
+    set altura(valor){
+        if(typeof valor !== "number" || valor <0){
+            throw new Error("Erro: a altura precisa ser numérica")
+        }
+        this.#altura = valor
+    }
+
+    set tipo(valor){
+        if(!["R", "T","E"].includes(valor.toUpperCase())){
+            throw new Error("ERRO: o tipo precisa ser um dos valores: R, T ou E ");   
+        }
+        this.#tipo = valor.toUpperCase()
+    }
+
+    calcArea(){ 
+        switch(this.tipo){
+            case "R": 
+            return this.#base * this.#altura
+
+            case "T":
+                return (this.#base * this.#altura) / 2
+            
+            case "E":
+                return (this.#base/2) *( this.#altura/2) * Math.PI
+        }
+    }
+}
